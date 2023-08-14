c3d5c16ee67f642fe5590e6f78681cfae22e077b
Xblade9 <57889674+Xblade9@users.noreply.github.com>
2023-08-14 16:20:51 +0530

    5 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { MatTabsModule, MatTabGroup } from '@angular/material';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular 5';
+
+  selected = 0; //by deafult report tab will be selected ...
+
+  tabType: string = 'Reports';
+
+  onselectTab(event) {
+    if (event.tab.textLabel === 'Tracker') {
+      this.tabType = 'Tracker';
+      console.log(this.tabType);
+    } else {
+      this.tabType = 'Reports';
+      console.log(this.tabType);
+    }
+  }
 }
